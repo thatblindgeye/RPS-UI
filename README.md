f8d54c4e59aca77ca06c1f6f9a62bceadea302b6
thatblindgeye <thatblindgeye@gmail.com>
2020-10-14 19:00:48 -0400

    Add javascript, Update css

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-This is my UI version of the Rock, Paper, Scissors project, using the code from my console version
-as a base and reworking it to be played in a browser window without the console.
+This is my UI version of the Rock, Paper, Scissors project, using the code from my console version as a base and reworking it to be played in a browser window without the console.
 
 Visit the following Github pages URL to play my UI version of <a href="">Rock, Paper, Scissors</a>.
--- script.js
+++ script.js
@@ -1,170 +1,111 @@
-// Global variable declarations
-let playerScore = 0;
-let computerScore = 0;
-let scoreCompare = 0;
-let gameCancel = false;
+let userScore = 0;
+let cpuScore = 0;
+let winningScore;
+const gameChoice = document.querySelectorAll(".game-btn");
+let timeout;
+let dialogue = document.querySelector("#dialogue");
+const userChoice = document.querySelectorAll(".user-choice");
 
-// Random generator for Computer's selection
-function computerPlay() {
-  const computerRandom = ["rock", "paper", "scissors"];
-  return computerRandom[Math.floor(Math.random() * computerRandom.length)];
-}
 
-/* Compare Player's selection to Computer's selection; a scoreCompare
-of null prevents round# from increasing on invalid inputs */
-function playRound(playerSelection, computerSelection) {
-  // Outcome for canceling game prompt
-  if (playerSelection == null) {
-    gameCancel = true;
-    return "Too scared to finish the game, huh?";
-  }
-
-  // Outcomes for Player making valid selection
-  else if (playerSelection != null) {
-    if (playerSelection.toLowerCase() == computerSelection) {
-      scoreCompare = playerScore + computerScore;
-      return "A draw?! That seems pretty sus...";
-    }
-
-    // Outcomes for Player selecting Rock
-    else if (playerSelection.toLowerCase() == "rock") {
-      if (computerSelection == "paper") {
-        ++computerScore;
-        scoreCompare = playerScore + computerScore;
-        return "My Paper covers your Rock! You lose this round, rocks-for-brains!";
-      } else if (computerSelection == "scissors") {
-        ++playerScore;
-        scoreCompare = playerScore + computerScore;
-        return (
-          "Your Rock broke my Scissors! You win this round," +
-          " but you owe me for damages..."
-        );
-      }
-    }
-    // Outcomes for Player selecting Paper
-    else if (playerSelection.toLowerCase() == "paper") {
-      if (computerSelection == "scissors") {
-        ++computerScore;
-        scoreCompare = playerScore + computerScore;
-        return (
-          "My Scissors cuts your Paper! You lose this round," +
-          " and I hope those papers were important!"
-        );
-      } else if (computerSelection == "rock") {
-        ++playerScore;
-        scoreCompare = playerScore + computerScore;
-        return (
-          "Your Paper covers my pet Rock! You win this round," +
-          " now let Rocky go!"
-        );
-      }
-    }
-    // Outcomes for Player selecting Scissors
-    else if (playerSelection.toLowerCase() == "scissors") {
-      if (computerSelection == "rock") {
-        ++computerScore;
-        scoreCompare = playerScore + computerScore;
-        return (
-          "My Rock breaks your puny Scissors! You lose this round," +
-          " good luck clipping coupons now!"
-        );
-      } else if (computerSelection == "paper") {
-        ++playerScore;
-        scoreCompare = playerScore + computerScore;
-        return (
-          "Your Scissors can cut my Paper?! You win this round," +
-          " but at least I have confetti now..."
-        );
-      }
-    }
-    // Outcome for Player making invalid selection
-    else {
-      scoreCompare = null;
-      return "Only valid options are allowed! Since I'm generous, you can try again.";
-    }
-  }
-}
-
-/* Reset variables to default values after 
-a game is completed or cancelled */
 function resetGame() {
-  playerScore = computerScore = scoreCompare = 0;
-  gameCancel = false;
+  userScore = cpuScore = 0;
+  document.querySelector(".user-score").textContent = `${userScore}`;
+  document.querySelector(".cpu-score").textContent = `${cpuScore}`;
 }
 
-// Show the results of a game after a player reaches 5 points
-function showResults() {
-  if (playerScore > computerScore) {
-    console.log(
-      `______ GAME OVER ______\n` +
-        `Congratulations! You cheated to win the game!\n\n`
-    );
-  } else if (computerScore > playerScore) {
-    console.log(
-      `______ GAME OVER ______\n` +
-        `Ha! You lost the game! Better luck next time!\n\n`
-    );
-  } else if (playerScore == computerScore) {
-    console.log(
-      `______ GAME OVER ______\n` + `We TIED?! That shouldn't be possible!\n\n`
-    );
+function chooseGame(e) {
+  resetGame();
+  clearTimeout(timeout);
+
+  dialogue.textContent = `So you think you can defeat me ${e.target.dataset.rounds} times? I will enjoy the... "challenge". Click a choice below to begin the game.`;
+
+  winningScore = Number(e.target.dataset.rounds);
+  e.target.classList.add("btn-press");
+}
+
+function deselectGame() {
+  gameChoice.forEach((gameChoice) => {
+    gameChoice.classList.remove("btn-press");
+  })
+}
+
+gameChoice.forEach((gameChoice) => {
+  gameChoice.addEventListener("click", (e) => {
+    deselectGame();
+    chooseGame(e);
+  })
+});
+
+
+function selectComputer() {
+  const randomChoice = ["rock", "paper", "scissors"];
+  return randomChoice[Math.floor(Math.random() * randomChoice.length)];
+}
+
+/* Below redundant while using :active in CSS?
+
+function pressButton(event) {
+  event.target.classList.add("btn-press");
+}
+
+function unpressButton(event) {
+  if (event.propertyName !== 'transform') return;
+  event.target.classList.remove('btn-press');
+} */
+
+function playRound(event) {
+  let cpuSelection = selectComputer();
+  let userSelection = event.target.id;
+  
+  if (userSelection === cpuSelection) {
+    dialogue.textContent = `It would seem we both chose ${userSelection}.`;
+  } else if ( userSelection === "rock" && cpuSelection === "scissors" ||
+    userSelection === "paper" && cpuSelection === "rock" || 
+    userSelection === "scissors" && cpuSelection === "paper") {
+      userScore++;
+      document.querySelector(".user-score").textContent = `${userScore}`;
+      dialogue.textContent = `Your ${userSelection} beats my ${cpuSelection}.`;
+  } else if ( userSelection === "rock" && cpuSelection === "paper" ||
+  userSelection === "paper" && cpuSelection === "scissors" || 
+  userSelection === "scissors" && cpuSelection === "rock") {
+    cpuScore++;
+    document.querySelector(".cpu-score").textContent = `${cpuScore}`;
+    dialogue.textContent = `My ${cpuSelection} beats your ${userSelection}.`;
+  } 
+  checkWinner();
+}
+
+function checkWinner() {
+  if (userScore === winningScore) {
+    dialogue.textContent = `WARNING: UNAUTHORIZED ACCESS TO MAINFRAME!`;
+
+    timeout = setTimeout(() => {dialogue.textContent = "Of course humans have to resort to cheating. Congratulations on your hollow victory. Click a game setting above for a rematch, maybe without cheating this time...";
+    }, 3000);
+  } else if (cpuScore === winningScore) {
+      dialogue.textContent = `Naturally it was easy to defeat you. Maybe you'll do better next time, not that I would count on it. Though if you really want to find out, then click a game setting above for a rematch.`;
   }
 }
 
-// Prompt to replay the game after finishing a game
-function replayGame() {
-  console.log("Want to play again?");
-  if (confirm("Play again?") == true) {
-    console.log(`Okay, let me just reset everything *BEEPBOOPBEEP*\n\n`);
-    resetGame();
-    return game();
-  } else {
-    console.log(
-      "Then I'll be waiting for you to find some courage for a rematch!"
-    );
-    resetGame();
-  }
-}
+/* Below redundant while using :active in CSS?
 
-// Play a 5 round game of Rock, Paper, Scissors
-function game() {
-  console.log(
-    `=====================\n` +
-      `ROCK, PAPER, SCISSORS\n\n` +
-      ` Get the most points\n` +
-      `     in 5 rounds\n` +
-      `=====================\n\n`
-  );
-  let round = 1;
-  while (round <= 5) {
-    if (scoreCompare !== null) {
-      console.log(`______ ROUND ${round} ______`);
-      round++;
+userChoice.forEach((userChoice) => {
+  userChoice.addEventListener("mousedown", (event) => {
+    if (userScore < winningScore && cpuScore < winningScore) {
+      pressButton(event);
     }
-    console.log(
-      playRound(prompt("Choose Rock, Paper, or Scissors:"), computerPlay())
-    );
+  })
+});
 
-    /* Return the scores after each round where Player
-    enters a valid input */
-    if (scoreCompare !== null && gameCancel == false) {
-      console.log(
-        `Human's Score:    ${playerScore}\n` +
-          `Computer's Score: ${computerScore}\n\n`
-      );
-    }
+userChoice.forEach((userChoice) => {
+  userChoice.addEventListener("transitionend", (event) => {
+    unpressButton(event);
+  })
+}); */
 
-    /* End the game early if Cancel is pressed on playerSelection,
-    resetting global variables to their defaults. */
-    if (gameCancel == true) {
-      resetGame();
-      break;
+userChoice.forEach((userChoice) => {
+  userChoice.addEventListener("click", (event) => {
+    if (userScore < winningScore && cpuScore < winningScore) {
+      playRound(event);
     }
-
-    // Show final results and prompt to replay the game
-    if (round > 5) {
-      showResults();
-      replayGame();
-    }
-  }
-}
+  })
+});
